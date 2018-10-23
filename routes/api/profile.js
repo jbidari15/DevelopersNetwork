5d6dba640ab86e1eff2deef27610f1e447ba08fc
Jaya Kumar Bidari <jaya.bidari@metropolia.fi>
2018-10-23 03:15:24 +0300

    added the delete route to delete Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -237,5 +237,17 @@ router.delete(
       .catch(err => res.status(404).json(err));
   }
 );
+//DELETE api/profile
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
 
 module.exports = router;
