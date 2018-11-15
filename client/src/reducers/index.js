769fd1196294e70c72d2614c0bd00392e05680df
Jaya Kumar Bidari <jaya.bidari@metropolia.fi>
2018-11-15 03:36:06 +0200

    added components for showing posts, adding comments , likes and dislikes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
 
 export const getErrors = state => state.errors;
@@ -16,3 +18,6 @@ export const getUser = state => state.auth.user;
 export const getProfile = state => state.profile.profile;
 export const getProfilesState = state => state.profile.profiles;
 export const getLoader = state => state.profile.loading;
+export const getallPosts = state => state.post.posts;
+export const getSinglePost = state => state.post.post;
+export const getPostLoader = state => state.post.loading;
