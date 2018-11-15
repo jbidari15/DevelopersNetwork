67082311f9df875e5b1589990f707a25466226eb
Jaya Kumar Bidari <jaya.bidari@metropolia.fi>
2018-11-15 04:41:54 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 const users = require("./routes/api/users");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
 
@@ -38,5 +39,14 @@ require("./config/passport")(passport);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 app.use("/api/users", users);
+
+//Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
